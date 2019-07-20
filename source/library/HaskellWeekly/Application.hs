fcf660760f764cc0b56c6df12096e06dc957b46f
Taylor Fausak <taylor@fausak.me>
2019-07-20 18:58:37 -0400

    Fix doctype shenanigans

--- source/library/HaskellWeekly/Application.hs
+++ source/library/HaskellWeekly/Application.hs
@@ -16,50 +16,53 @@ import qualified Network.HTTP.Types
 import qualified Network.Wai
 
 defaultHtml :: String -> Lucid.Html ()
-defaultHtml content = Lucid.doctype_ . Lucid.html_ [Lucid.class_ $ Data.Text.pack "b--purple bt bw2" ] $ do
-  Lucid.head_ $ do
-    Lucid.meta_ [Lucid.charset_ $ Data.Text.pack "utf-8"]
-    Lucid.meta_
-      [ Lucid.name_ $ Data.Text.pack "viewport"
-      , Lucid.content_
-        $ Data.Text.pack "initial-scale = 1, width = device-width"
-      ]
-    Lucid.title_ $ Lucid.toHtml "Haskell Weekly"
-    Lucid.link_
-      [ Lucid.href_ . Data.Text.pack $ HaskellWeekly.Type.Route.routeToString
-        HaskellWeekly.Type.Route.RouteTachyons
-      , Lucid.rel_ $ Data.Text.pack "stylesheet"
-      ]
-  Lucid.body_
-      [ Lucid.class_
-          $ Data.Text.pack "bg-white black flex justify-center ma3 sans-serif"
-      ]
-    . Lucid.div_ [Lucid.class_ $ Data.Text.pack "mw7 w-100"]
-    $ do
-        Lucid.header_ [Lucid.class_ $ Data.Text.pack "mv3"]
-          . Lucid.h1_ [Lucid.class_ $ Data.Text.pack "f1 tracked-tight"]
-          $ Lucid.toHtml "Haskell Weekly"
-        Lucid.main_ [Lucid.class_ $ Data.Text.pack "mv3"]
-          . Lucid.p_
-          $ Lucid.toHtml content
-        Lucid.footer_ [Lucid.class_ $ Data.Text.pack "gray mv3"]
-          . Lucid.p_
-          $ do
-              Lucid.toHtml "Content on this site is licensed under a "
-              Lucid.a_
-                  [ Lucid.class_ $ Data.Text.pack "color-inherit"
-                  , Lucid.href_ $ Data.Text.pack
-                    "https://creativecommons.org/licenses/by/4.0/"
-                  ]
-                $ Lucid.toHtml "Creative Commons Attribution 4.0 International"
-              Lucid.toHtml " license. The "
-              Lucid.a_
-                  [ Lucid.class_ $ Data.Text.pack "color-inherit"
-                  , Lucid.href_ $ Data.Text.pack
-                    "https://github.com/haskellweekly/haskellweekly"
-                  ]
-                $ Lucid.toHtml "source code"
-              Lucid.toHtml " for this site is available on GitHub."
+defaultHtml content = do
+  Lucid.doctype_
+  Lucid.html_ [Lucid.class_ $ Data.Text.pack "b--purple bt bw2"] $ do
+    Lucid.head_ $ do
+      Lucid.meta_ [Lucid.charset_ $ Data.Text.pack "utf-8"]
+      Lucid.meta_
+        [ Lucid.name_ $ Data.Text.pack "viewport"
+        , Lucid.content_
+          $ Data.Text.pack "initial-scale = 1, width = device-width"
+        ]
+      Lucid.title_ $ Lucid.toHtml "Haskell Weekly"
+      Lucid.link_
+        [ Lucid.href_ . Data.Text.pack $ HaskellWeekly.Type.Route.routeToString
+          HaskellWeekly.Type.Route.RouteTachyons
+        , Lucid.rel_ $ Data.Text.pack "stylesheet"
+        ]
+    Lucid.body_
+        [ Lucid.class_ $ Data.Text.pack
+            "bg-white black flex justify-center ma3 sans-serif"
+        ]
+      . Lucid.div_ [Lucid.class_ $ Data.Text.pack "mw7 w-100"]
+      $ do
+          Lucid.header_ [Lucid.class_ $ Data.Text.pack "mv3"]
+            . Lucid.h1_ [Lucid.class_ $ Data.Text.pack "f1 tracked-tight"]
+            $ Lucid.toHtml "Haskell Weekly"
+          Lucid.main_ [Lucid.class_ $ Data.Text.pack "mv3"]
+            . Lucid.p_
+            $ Lucid.toHtml content
+          Lucid.footer_ [Lucid.class_ $ Data.Text.pack "gray mv3"]
+            . Lucid.p_
+            $ do
+                Lucid.toHtml "Content on this site is licensed under a "
+                Lucid.a_
+                    [ Lucid.class_ $ Data.Text.pack "color-inherit"
+                    , Lucid.href_ $ Data.Text.pack
+                      "https://creativecommons.org/licenses/by/4.0/"
+                    ]
+                  $ Lucid.toHtml
+                      "Creative Commons Attribution 4.0 International"
+                Lucid.toHtml " license. The "
+                Lucid.a_
+                    [ Lucid.class_ $ Data.Text.pack "color-inherit"
+                    , Lucid.href_ $ Data.Text.pack
+                      "https://github.com/haskellweekly/haskellweekly"
+                    ]
+                  $ Lucid.toHtml "source code"
+                Lucid.toHtml " for this site is available on GitHub."
 
 application :: HaskellWeekly.Type.State.State -> Network.Wai.Application
 application state request respond = do
