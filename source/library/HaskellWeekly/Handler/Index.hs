baf80cb7a2bf147d284e94e895d70aa7ff6592b2
Taylor Fausak <taylor@fausak.me>
2019-07-22 22:49:17 -0400

    Add a common base template

--- source/library/HaskellWeekly/Handler/Index.hs
+++ source/library/HaskellWeekly/Handler/Index.hs
@@ -4,10 +4,9 @@ module HaskellWeekly.Handler.Index
 where
 
 import qualified Data.String
-import qualified Data.Text
 import qualified Database.PostgreSQL.Simple
 import qualified HaskellWeekly.Handler.Base
-import qualified HaskellWeekly.Type.Route
+import qualified HaskellWeekly.Template.Base
 import qualified HaskellWeekly.Type.State
 import qualified Lucid
 import qualified Network.HTTP.Types
@@ -24,51 +23,4 @@ indexHandler state = do
     html
 
 html :: Lucid.Html ()
-html = do
-  Lucid.doctype_
-  Lucid.html_ [Lucid.class_ $ Data.Text.pack "b--purple bt bw2"] $ do
-    Lucid.head_ $ do
-      Lucid.meta_ [Lucid.charset_ $ Data.Text.pack "utf-8"]
-      Lucid.meta_
-        [ Lucid.name_ $ Data.Text.pack "viewport"
-        , Lucid.content_
-          $ Data.Text.pack "initial-scale = 1, width = device-width"
-        ]
-      Lucid.title_ $ Lucid.toHtml "Haskell Weekly"
-      Lucid.link_
-        [ Lucid.href_ . Data.Text.pack $ HaskellWeekly.Type.Route.routeToString
-          HaskellWeekly.Type.Route.RouteTachyons
-        , Lucid.rel_ $ Data.Text.pack "stylesheet"
-        ]
-    Lucid.body_
-        [ Lucid.class_ $ Data.Text.pack
-            "bg-white black flex justify-center ma3 sans-serif"
-        ]
-      . Lucid.div_ [Lucid.class_ $ Data.Text.pack "mw7 w-100"]
-      $ do
-          Lucid.header_ [Lucid.class_ $ Data.Text.pack "mv3"]
-            . Lucid.h1_ [Lucid.class_ $ Data.Text.pack "f1 tracked-tight"]
-            $ Lucid.toHtml "Haskell Weekly"
-          Lucid.main_ [Lucid.class_ $ Data.Text.pack "mv3"]
-            . Lucid.p_
-            $ Lucid.toHtml "200 OK"
-          Lucid.footer_ [Lucid.class_ $ Data.Text.pack "gray mv3"]
-            . Lucid.p_
-            $ do
-                Lucid.toHtml "The content on this site is licensed under a "
-                Lucid.a_
-                    [ Lucid.class_ $ Data.Text.pack "color-inherit"
-                    , Lucid.href_ $ Data.Text.pack
-                      "https://creativecommons.org/licenses/by/4.0/"
-                    ]
-                  $ Lucid.toHtml
-                      "Creative Commons Attribution 4.0 International"
-                Lucid.toHtml
-                  " license. The code that powers this site is available "
-                Lucid.a_
-                    [ Lucid.class_ $ Data.Text.pack "color-inherit"
-                    , Lucid.href_ $ Data.Text.pack
-                      "https://github.com/haskellweekly/haskellweekly"
-                    ]
-                  $ Lucid.toHtml "on GitHub"
-                Lucid.toHtml "."
+html = HaskellWeekly.Template.Base.baseTemplate [] $ Lucid.toHtml "200 OK"
---
+++ source/library/HaskellWeekly/Template/Base.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module HaskellWeekly.Template.Base
+  ( baseTemplate
+  )
+where
+
+import qualified Data.List
+import qualified Data.Text
+import qualified HaskellWeekly.Type.Route
+import qualified Lucid as H
+
+baseTemplate :: [String] -> H.Html () -> H.Html ()
+baseTemplate title body = do
+  H.doctype_
+  H.html_ [H.class_ "b--purple bt bw2"] $ do
+    H.head_ $ do
+      H.meta_ [H.charset_ "utf-8"]
+      H.meta_
+        [ H.name_ "viewport"
+        , H.content_ "initial-scale = 1, width = device-width"
+        ]
+      H.title_ . H.toHtml $ Data.List.intercalate
+        " \x2192 "
+        ("Haskell Weekly" : title)
+      H.link_
+        [ H.href_ . Data.Text.pack $ HaskellWeekly.Type.Route.routeToString
+          HaskellWeekly.Type.Route.RouteTachyons
+        , H.rel_ "stylesheet"
+        ]
+    H.body_ [H.class_ "bg-white black flex justify-center ma3 sans-serif"]
+      . H.div_ [H.class_ "mw7 w-100"]
+      $ do
+          H.header_ [H.class_ "mv3"]
+            $ H.h1_ [H.class_ "f1 tracked-tight"] "Haskell Weekly"
+          H.main_ [H.class_ "mv3"] body
+          H.footer_ [H.class_ "gray mv3"] . H.p_ $ do
+            "The content on this site is licensed under a "
+            H.a_
+              [ H.class_ "color-inherit"
+              , H.href_ "https://creativecommons.org/licenses/by/4.0/"
+              ]
+              "Creative Commons Attribution 4.0 International"
+            " license. The code that powers this site is available "
+            H.a_
+              [ H.class_ "color-inherit"
+              , H.href_ "https://github.com/haskellweekly/haskellweekly"
+              ]
+              "on GitHub"
+            "."
