ad98647b8fb897995f68478f6e3c8058740bb259
Taylor Fausak <taylor@fausak.me>
2019-08-04 14:55:28 -0400

    Move advertising template into its own file

--- source/library/HaskellWeekly/Handler/Advertising.hs
+++ source/library/HaskellWeekly/Handler/Advertising.hs
@@ -1,51 +1,15 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module HaskellWeekly.Handler.Advertising
   ( advertisingHandler
   )
 where
 
 import qualified HaskellWeekly.Handler.Base
-import qualified HaskellWeekly.Template.Base
-import qualified Lucid as H
+import qualified HaskellWeekly.Template.Advertising
 import qualified Network.HTTP.Types
 import qualified Network.Wai
 
 advertisingHandler :: Applicative f => f Network.Wai.Response
-advertisingHandler = pure
-  $ HaskellWeekly.Handler.Base.htmlResponse Network.HTTP.Types.ok200 [] html
-
-html :: H.Html ()
-html = HaskellWeekly.Template.Base.baseTemplate ["Advertising"] $ do
-  H.h2_ [H.class_ "f2"] "Advertising"
-  H.p_ $ do
-    "You can advertise with Haskell Weekly by purchasing a sponsored link. "
-    "Sponsored links are similar to featured links except that they say '(ad)' after the title. "
-    "Here's an example of how they look: "
-  H.ul_ . H.li_ $ do
-    H.p_ $ do
-      H.a_
-        [H.href_ "#"]
-        "Reach an engaged audience by advertising with Haskell Weekly!"
-      " (ad)"
-    H.p_ $ do
-      "As of May 2019, over 3,300 people subscribe to Haskell Weekly. "
-      "Over the last five issues, the average open rate was 50% and the average click rate was 21%."
-  H.p_ "Each sponsored link has three required pieces:"
-  H.ol_ $ do
-    H.li_ . H.p_ $ do
-      "URL: This can be whatever you want. "
-      "We recommend using a URL that will allow you to track engagement."
-    H.li_ $ H.p_ "Title: This is up to 80 characters of plain text."
-    H.li_ . H.p_ $ do
-      "Copy: This is up to 300 characters of plain text. "
-      "It can include basic Markdown formatting."
-  H.p_ $ do
-    "Haskell Weekly is published at 10 AM ET every Thursday. "
-    "Sponsored links must be submitted 24 hours before publication."
-  H.p_ $ do
-    "Sponsored links cost US$60 per issue. "
-    "Payment is accepted through Square. "
-    "If you are interesting in advertising with Haskell Weekly, please reach out to "
-    H.a_ [H.href_ "mailto:info@haskellweekly.news"] "info@haskellweekly.news"
-    "."
+advertisingHandler = pure $ HaskellWeekly.Handler.Base.htmlResponse
+  Network.HTTP.Types.ok200
+  []
+  HaskellWeekly.Template.Advertising.advertisingTemplate
