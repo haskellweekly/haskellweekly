f773061aedbeaaddd9e7e8ce357c97fdcfe32545
Taylor Fausak <taylor@fausak.me>
2019-08-04 14:43:01 -0400

    Improve advertising template

--- source/library/HaskellWeekly/Handler/Advertising.hs
+++ source/library/HaskellWeekly/Handler/Advertising.hs
@@ -1,12 +1,13 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module HaskellWeekly.Handler.Advertising
   ( advertisingHandler
   )
 where
 
-import qualified Data.Text
 import qualified HaskellWeekly.Handler.Base
 import qualified HaskellWeekly.Template.Base
-import qualified Lucid
+import qualified Lucid as H
 import qualified Network.HTTP.Types
 import qualified Network.Wai
 
@@ -14,38 +15,37 @@ advertisingHandler :: Applicative f => f Network.Wai.Response
 advertisingHandler = pure
   $ HaskellWeekly.Handler.Base.htmlResponse Network.HTTP.Types.ok200 [] html
 
-html :: Lucid.Html ()
+html :: H.Html ()
 html = HaskellWeekly.Template.Base.baseTemplate ["Advertising"] $ do
-  Lucid.h2_ [Lucid.class_ $ Data.Text.pack "f2"] $ Lucid.toHtml "Advertising"
-  Lucid.p_
-    $ Lucid.toHtml
-        " You can advertise with Haskell Weekly by purchasing a sponsored link. Sponsored links are similar to featured links except that they say \"(ad)\" after the title. Here's an example of how they look: "
-  Lucid.ul_ . Lucid.li_ $ do
-    Lucid.p_ $ do
-      Lucid.a_ [Lucid.href_ $ Data.Text.pack "#"] $ Lucid.toHtml
+  H.h2_ [H.class_ "f2"] "Advertising"
+  H.p_ $ do
+    "You can advertise with Haskell Weekly by purchasing a sponsored link. "
+    "Sponsored links are similar to featured links except that they say '(ad)' after the title. "
+    "Here's an example of how they look: "
+  H.ul_ . H.li_ $ do
+    H.p_ $ do
+      H.a_
+        [H.href_ "#"]
         "Reach an engaged audience by advertising with Haskell Weekly!"
-      Lucid.toHtml " (ad)"
-    Lucid.p_
-      $ Lucid.toHtml
-          "As of May 2019, over 3,300 people subscribe to Haskell Weekly. Over the last five issues, the average open rate was 50% and the average click rate was 21%."
-  Lucid.p_ $ Lucid.toHtml "Each sponsored link has three required pieces:"
-  Lucid.ol_ $ do
-    Lucid.li_
-      . Lucid.p_
-      $ Lucid.toHtml
-          "URL: This can be whatever you want. We recommend using a URL that will allow you to track engagement."
-    Lucid.li_ . Lucid.p_ $ Lucid.toHtml
-      "Title: This is up to 80 characters of plain text."
-    Lucid.li_
-      . Lucid.p_
-      $ Lucid.toHtml
-          "Copy: This is up to 300 characters of plain text. It can include basic Markdown formatting."
-  Lucid.p_
-    $ Lucid.toHtml
-        "Haskell Weekly is published at 10 AM ET every Thursday. Sponsored links must be submitted 24 hours before publication."
-  Lucid.p_ $ do
-    Lucid.toHtml
-      "Sponsored links cost US$60 per issue. Payment is accepted through Square. If you are interesting in advertising with Haskell Weekly, please reach out to "
-    Lucid.a_ [Lucid.href_ $ Data.Text.pack "mailto:info@haskellweekly.news"]
-      $ Lucid.toHtml "info@haskellweekly.news"
-    Lucid.toHtml "."
+      " (ad)"
+    H.p_ $ do
+      "As of May 2019, over 3,300 people subscribe to Haskell Weekly. "
+      "Over the last five issues, the average open rate was 50% and the average click rate was 21%."
+  H.p_ "Each sponsored link has three required pieces:"
+  H.ol_ $ do
+    H.li_ . H.p_ $ do
+      "URL: This can be whatever you want. "
+      "We recommend using a URL that will allow you to track engagement."
+    H.li_ $ H.p_ "Title: This is up to 80 characters of plain text."
+    H.li_ . H.p_ $ do
+      "Copy: This is up to 300 characters of plain text. "
+      "It can include basic Markdown formatting."
+  H.p_ $ do
+    "Haskell Weekly is published at 10 AM ET every Thursday. "
+    "Sponsored links must be submitted 24 hours before publication."
+  H.p_ $ do
+    "Sponsored links cost US$60 per issue. "
+    "Payment is accepted through Square. "
+    "If you are interesting in advertising with Haskell Weekly, please reach out to "
+    H.a_ [H.href_ "mailto:info@haskellweekly.news"] "info@haskellweekly.news"
+    "."
