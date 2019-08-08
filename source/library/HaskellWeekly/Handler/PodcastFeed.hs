f8512279cc9b7245d394cd36d75048790527a80f
Taylor Fausak <taylor@fausak.me>
2019-08-08 18:57:50 -0400

    Don't bother sorting episodes for the feed

--- source/library/HaskellWeekly/Handler/PodcastFeed.hs
+++ source/library/HaskellWeekly/Handler/PodcastFeed.hs
@@ -3,13 +3,10 @@ module HaskellWeekly.Handler.PodcastFeed
   )
 where
 
-import qualified Data.List
 import qualified Data.Map
-import qualified Data.Ord
 import qualified HaskellWeekly.Handler.Base
 import qualified HaskellWeekly.Template.PodcastFeed
 import qualified HaskellWeekly.Type.Config
-import qualified HaskellWeekly.Type.Episode
 import qualified HaskellWeekly.Type.State
 import qualified Network.HTTP.Types
 import qualified Network.Wai
@@ -20,10 +17,7 @@ podcastFeedHandler state =
   let
     baseUrl = HaskellWeekly.Type.Config.configBaseUrl
       $ HaskellWeekly.Type.State.stateConfig state
-    episodes =
-      Data.List.sortOn (Data.Ord.Down . HaskellWeekly.Type.Episode.episodeDate)
-        . Data.Map.elems
-        $ HaskellWeekly.Type.State.stateEpisodes state
+    episodes = Data.Map.elems $ HaskellWeekly.Type.State.stateEpisodes state
   in
     pure
     . HaskellWeekly.Handler.Base.feedResponse Network.HTTP.Types.ok200 []
