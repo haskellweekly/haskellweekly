7649dbdcdfe512ddfa72c209e57e45a00f7ddafe
Taylor Fausak <taylor@fausak.me>
2019-09-28 09:39:47 -0400

    Improve the newsletter index

--- source/library/HaskellWeekly/Template/Newsletter.hs
+++ source/library/HaskellWeekly/Template/Newsletter.hs
@@ -11,29 +11,58 @@ import qualified HaskellWeekly.Type.Issue
 import qualified HaskellWeekly.Type.Number
 import qualified HaskellWeekly.Type.Route
 import qualified Lucid as H
+import qualified Lucid.Base as H
 
 newsletterTemplate :: String -> [HaskellWeekly.Type.Issue.Issue] -> H.Html ()
 newsletterTemplate baseUrl issues =
   HaskellWeekly.Template.Base.baseTemplate baseUrl [] $ do
     H.h2_ [H.class_ "f2 mv3 tracked-tight"] "Newsletter"
-    H.p_ $ do
+    H.p_ [H.class_ "lh-copy"] $ do
       "The Haskell Weekly Newsletter covers the Haskell programming langauge. "
       "Each issue features several hand-picked links to interesting content about Haskell from around the web."
-    H.p_ $ do
-      "Subscribe now through "
-      H.a_
-        [ H.href_
-            "https://news.us10.list-manage.com/subscribe?u=49a6a2e17b12be2c5c4dcb232&id=ffbbbbd930"
-        ]
-        "Mailchimp"
-      " or subscribe to the "
-      H.a_
-        [ H.href_ $ HaskellWeekly.Type.Route.routeToTextWith
-            baseUrl
-            HaskellWeekly.Type.Route.RouteNewsletterFeed
-        ]
-        "RSS feed"
-      "."
+    H.div_ [H.class_ "ba b--yellow bg-washed-yellow center mw6 pa3"] $ do
+      H.p_ [H.class_ "lh-copy mt0"] $ do
+        "Subscribe now to get a newsletter delivered to your inbox every Thursday. "
+        "It's easy to unsubscribe and we'll never send you spam. "
+        "If you'd prefer, you can follow us "
+        H.a_ [H.href_ "https://twitter.com/haskellweekly"] "on Twitter"
+        " or subscribe to "
+        H.a_
+          [ H.href_ $ HaskellWeekly.Type.Route.routeToTextWith
+              baseUrl
+              HaskellWeekly.Type.Route.RouteNewsletterFeed
+          ]
+          "the feed"
+        "."
+      H.form_
+          [ H.action_
+            "https://news.us10.list-manage.com/subscribe/post?u=49a6a2e17b12be2c5c4dcb232&id=ffbbbbd930"
+          , H.method_ "post"
+          ]
+        $ do
+            H.div_
+                [ H.makeAttribute "aria-hidden" "true"
+                , H.class_ "absolute top--2"
+                ]
+              $ H.input_
+                  [ H.name_ "b_49a6a2e17b12be2c5c4dcb232_ffbbbbd930"
+                  , H.tabindex_ "-1"
+                  , H.type_ "text"
+                  , H.value_ ""
+                  ]
+            H.input_
+              [ H.class_ "ba b--silver db input-reset pa3 w-100"
+              , H.name_ "EMAIL"
+              , H.placeholder_ "you@example.com"
+              , H.required_ "required"
+              , H.type_ "email"
+              ]
+            H.button_
+              [ H.class_
+                "b ba b--dark-blue bg-blue center db input-reset mt3 pa3 pointer white"
+              , H.type_ "submit"
+              ]
+              "Subscribe"
     H.ul_ [H.class_ "lh-copy"] $ mapM_ (issueTemplate baseUrl) issues
 
 issueTemplate :: String -> HaskellWeekly.Type.Issue.Issue -> H.Html ()
