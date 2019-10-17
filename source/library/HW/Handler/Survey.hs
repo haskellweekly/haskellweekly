616a95b0c721b43113b95c680d961758d6ec3f6c
Taylor Fausak <taylor@fausak.me>
2019-10-17 19:29:13 -0400

    Don't cache the latest survey

--- source/library/HW/Handler/Survey.hs
+++ source/library/HW/Handler/Survey.hs
@@ -23,15 +23,21 @@ surveyHandler number = do
   state <- HW.Type.App.getState
   let baseUrl = HW.Type.Config.configBaseUrl $ HW.Type.State.stateConfig state
   case HW.Type.Number.numberToNatural number of
-    2017 -> respondWith $ HW.Template.Survey2017.survey2017Template baseUrl
-    2018 -> respondWith $ HW.Template.Survey2018.survey2018Template baseUrl
+    2017 ->
+      respondWith [(Network.HTTP.Types.hCacheControl, "public, max-age=900")]
+        $ HW.Template.Survey2017.survey2017Template baseUrl
+    2018 ->
+      respondWith [(Network.HTTP.Types.hCacheControl, "public, max-age=900")]
+        $ HW.Template.Survey2018.survey2018Template baseUrl
     2019 -> do
       guid <- Control.Monad.IO.Class.liftIO
         $ fmap HW.Type.Guid.uuidToGuid System.Random.randomIO
-      respondWith $ HW.Template.Survey2019.survey2019Template baseUrl guid
+      respondWith [] $ HW.Template.Survey2019.survey2019Template baseUrl guid
     _ -> pure HW.Handler.Base.notFoundResponse
 
-respondWith :: Lucid.Html () -> HW.Type.App.App Network.Wai.Response
-respondWith = pure . HW.Handler.Base.htmlResponse
-  Network.HTTP.Types.ok200
-  [(Network.HTTP.Types.hCacheControl, "public, max-age=900")]
+respondWith
+  :: Network.HTTP.Types.ResponseHeaders
+  -> Lucid.Html ()
+  -> HW.Type.App.App Network.Wai.Response
+respondWith headers =
+  pure . HW.Handler.Base.htmlResponse Network.HTTP.Types.ok200 headers
