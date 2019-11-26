34bc99241185994e099c22ef0b45a0a1e82813f9
Taylor Fausak <taylor@fausak.me>
2019-11-25 21:35:19 -0500

    Revert "Ignore certain domain names"
    This reverts commit 4fbd4540098c3d94126160bb7c416e365d41b72c.

--- source/library/HW/Worker.hs
+++ source/library/HW/Worker.hs
@@ -22,19 +22,11 @@ import qualified Network.URI
 
 worker :: Data.IORef.IORef HW.Type.State.State -> IO ()
 worker stateRef = do
-  urls <- getUrls stateRef
-  print $ Data.Set.size urls
-  Control.Monad.forever $ Control.Concurrent.threadDelay 1000000
-
-getUrls
-  :: Data.IORef.IORef HW.Type.State.State -> IO (Data.Set.Set Network.URI.URI)
-getUrls stateRef = do
   state <- Data.IORef.readIORef stateRef
   let urlsFromEpisodes = getUrlsFromEpisodes state
   urlsFromIssues <- getUrlsFromIssues stateRef
-  pure . Data.Set.filter (not . shouldIgnore) $ Data.Set.union
-    urlsFromEpisodes
-    urlsFromIssues
+  print . Data.Set.size $ Data.Set.union urlsFromEpisodes urlsFromIssues
+  Control.Monad.forever $ Control.Concurrent.threadDelay 1000000
 
 getUrlsFromEpisodes :: HW.Type.State.State -> Data.Set.Set Network.URI.URI
 getUrlsFromEpisodes =
@@ -75,33 +67,3 @@ getUrlsFromNode (CMark.Node _ nodeType nodes) =
     case nodeType of
       CMark.LINK url _ -> url : rest
       _ -> rest
-
-shouldIgnore :: Network.URI.URI -> Bool
-shouldIgnore =
-  maybe
-      True
-      (flip Data.Set.member domainNamesToIgnore . Network.URI.uriRegName)
-    . Network.URI.uriAuthority
-
--- | These domains are frequently linked to, but they either don't have or
--- don't support Atom/RSS feeds. Instead of repeatedly discovering that, we'll
--- simply avoid crawling them at all.
-domainNamesToIgnore :: Data.Set.Set String
-domainNamesToIgnore = Data.Set.fromList
-  [ "discourse.haskell.org"
-  , "docs.google.com"
-  , "ghc.haskell.org"
-  , "gist.github.com"
-  , "github.com"
-  , "hackage.haskell.org"
-  , "mail.haskell.org"
-  , "medium.com" -- has RSS feeds, but not <link>ed
-  , "np.reddit.com"
-  , "stackoverflow.com"
-  , "twitter.com"
-  , "wiki.haskell.org"
-  , "www.facebook.com"
-  , "www.linkedin.com"
-  , "www.meetup.com"
-  , "www.youtube.com"
-  ]
