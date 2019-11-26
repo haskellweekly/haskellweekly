ab24a31c72810f5b82dc20c084210bc4889eed48
Taylor Fausak <taylor@fausak.me>
2019-11-25 21:35:02 -0500

    Revert "Extract feed URLs from linked pages"
    This reverts commit b1362dc67d38041362f538526fe6795e25451d23.

--- source/library/HW/Worker.hs
+++ source/library/HW/Worker.hs
@@ -8,17 +8,13 @@ import qualified Control.Concurrent
 import qualified Control.Exception
 import qualified Control.Monad
 import qualified Control.Monad.Trans.Reader
-import qualified Data.ByteString.Lazy
 import qualified Data.Either
 import qualified Data.IORef
-import qualified Data.List
 import qualified Data.List.NonEmpty
 import qualified Data.Map
-import qualified Data.Maybe
 import qualified Data.Pool
 import qualified Data.Set
-import qualified Data.Text.Encoding
-import qualified Data.Text.Encoding.Error
+import qualified Data.Text
 import qualified Database.PostgreSQL.Simple
 import qualified HW.Handler.Issue
 import qualified HW.Type.Article
@@ -28,22 +24,14 @@ import qualified HW.Type.Number
 import qualified HW.Type.State
 import qualified Network.HTTP.Client
 import qualified Network.URI
-import qualified Text.HTML.TagSoup
-import qualified Text.StringLike
 
 worker :: Data.IORef.IORef HW.Type.State.State -> IO ()
 worker stateRef = do
   urls <- getUrls stateRef
-  updateFeedUrls stateRef urls
-  Control.Monad.forever $ Control.Concurrent.threadDelay 1000000
-
-updateFeedUrls
-  :: Data.IORef.IORef HW.Type.State.State
-  -> Data.Set.Set Network.URI.URI
-  -> IO ()
-updateFeedUrls stateRef urls = do
+  print $ Data.Set.size urls
   state <- Data.IORef.readIORef stateRef
   mapM_ (updateFeedUrl state) $ Data.Set.toList urls
+  Control.Monad.forever $ Control.Concurrent.threadDelay 1000000
 
 updateFeedUrl :: HW.Type.State.State -> Network.URI.URI -> IO ()
 updateFeedUrl state uri =
@@ -57,7 +45,7 @@ updateFeedUrl state uri =
       case rows of
         [[n]] | n == (1 :: Int) -> pure ()
         _ -> do
-          putStrLn $ "- " <> show uri
+          putStrLn $ Data.Text.unpack uriText
           request <- fmap Network.HTTP.Client.setRequestCheckStatus
             $ Network.HTTP.Client.requestFromURI uri
           result <- Control.Exception.try
@@ -71,55 +59,7 @@ updateFeedUrl state uri =
                 connection
                 "insert into feeds ( page_url ) values ( ? )"
                 [uriText]
-            Right response -> do
-              let feedUrl = extractFeedUrl uri response
-              Control.Monad.void $ Database.PostgreSQL.Simple.execute
-                connection
-                "insert into feeds ( page_url, feed_url ) values ( ?, ? )"
-                [Just uriText, fmap HW.Type.Article.uriToText feedUrl]
-
-extractFeedUrl
-  :: Network.URI.URI
-  -> Network.HTTP.Client.Response Data.ByteString.Lazy.ByteString
-  -> Maybe Network.URI.URI
-extractFeedUrl uri =
-  fmap (flip Network.URI.relativeTo uri)
-    . Data.Maybe.listToMaybe
-    . Data.Maybe.mapMaybe
-        (\tag -> do
-          href <- extractLinkHref tag
-          parseLinkHref href
-        )
-    . Data.List.sortOn isRssLink
-    . filter (\tag -> isAtomLink tag || isRssLink tag)
-    . Text.HTML.TagSoup.parseTags
-    . Data.Text.Encoding.decodeUtf8With Data.Text.Encoding.Error.lenientDecode
-    . Data.ByteString.Lazy.toStrict
-    . Network.HTTP.Client.responseBody
-
-parseLinkHref :: Text.StringLike.StringLike str => str -> Maybe Network.URI.URI
-parseLinkHref = Network.URI.parseURIReference . Text.StringLike.toString
-
-extractLinkHref
-  :: Text.StringLike.StringLike str => Text.HTML.TagSoup.Tag str -> Maybe str
-extractLinkHref tag = case tag of
-  Text.HTML.TagSoup.TagOpen "link" attributes -> lookup "href" attributes
-  _ -> Nothing
-
-isAtomLink
-  :: Text.StringLike.StringLike str => Text.HTML.TagSoup.Tag str -> Bool
-isAtomLink = tagMatches "<link rel='alternate' type='application/atom+xml'>"
-
-isRssLink
-  :: Text.StringLike.StringLike str => Text.HTML.TagSoup.Tag str -> Bool
-isRssLink = tagMatches "<link rel='alternate' type='application/rss+xml'>"
-
-tagMatches
-  :: Text.StringLike.StringLike str
-  => String
-  -> Text.HTML.TagSoup.Tag str
-  -> Bool
-tagMatches string tag = tag Text.HTML.TagSoup.~== string
+            Right _response -> pure () -- TODO
 
 getUrls
   :: Data.IORef.IORef HW.Type.State.State -> IO (Data.Set.Set Network.URI.URI)
