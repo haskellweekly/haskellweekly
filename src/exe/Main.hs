7bc7844a1cba6de316eef9d4efae93a4dde170b7
Taylor Fausak <taylor@fausak.me>
2020-10-10 14:10:59 +0000

    Avoid redefining "main"

--- src/exe/Main.hs
+++ src/exe/Main.hs
@@ -6,10 +6,4 @@ module Main
   )
 where
 
-import qualified HaskellWeekly
-
--- | The main Haskell Weekly executable is architected to be usable as a
--- library as well. That means all we have to do here is wire up this
--- executable to the library.
-main :: IO ()
-main = HaskellWeekly.main
+import HaskellWeekly (main)
