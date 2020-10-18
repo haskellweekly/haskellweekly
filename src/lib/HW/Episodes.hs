2470aa01f59a655023878afb5bd86169a5c3bd36
Taylor Fausak <taylor@fausak.me>
2020-10-18 19:40:52 -0400

    Use new media CloudFront distribution

--- src/lib/HW/Episodes.hs
+++ src/lib/HW/Episodes.hs
@@ -104,7 +104,7 @@ episode1 =
     <$> HW.Type.Articles.textsToArticles
           ["https://markkarpov.com/tutorial/exceptions.html"]
     <*> HW.Type.Audio.textToAudio
-          "https://d1irmm0k9ptj7n.cloudfront.net/2019-03-11-episode-1.mp3"
+          "https://media.haskell-weekly.com/2019-03-11-episode-1.mp3"
     <*> HW.Type.Date.gregorianToDate 2019 3 11
     <*> HW.Type.Duration.timestampToDuration 9 43
     <*> HW.Type.Guid.textToGuid "6fe12dba-e0c3-4af5-b9fc-844bc2396ae7"
@@ -120,7 +120,7 @@ episode2 =
     <$> HW.Type.Articles.textsToArticles
           ["https://engineering.itpro.tv/2019/03/01/upgrading-elm-to-v19/"]
     <*> HW.Type.Audio.textToAudio
-          "https://d1irmm0k9ptj7n.cloudfront.net/2019-03-18-episode-2.mp3"
+          "https://media.haskell-weekly.com/2019-03-18-episode-2.mp3"
     <*> HW.Type.Date.gregorianToDate 2019 3 18
     <*> HW.Type.Duration.timestampToDuration 14 59
     <*> HW.Type.Guid.textToGuid "00900298-5aa6-4301-a207-619d38cdc81a"
@@ -136,7 +136,7 @@ episode3 =
     <$> HW.Type.Articles.textsToArticles
           ["https://www.parsonsmatt.org/2015/10/03/elm_vs_purescript.html"]
     <*> HW.Type.Audio.textToAudio
-          "https://d1irmm0k9ptj7n.cloudfront.net/2019-03-25-episode-3.mp3"
+          "https://media.haskell-weekly.com/2019-03-25-episode-3.mp3"
     <*> HW.Type.Date.gregorianToDate 2019 3 25
     <*> HW.Type.Duration.timestampToDuration 23 47
     <*> HW.Type.Guid.textToGuid "069964f7-2457-479f-8bab-9cb4f3abec9c"
@@ -153,7 +153,7 @@ episode4 =
           [ "https://runtimeverification.com/blog/code-smell-boolean-blindness/"
           ]
     <*> HW.Type.Audio.textToAudio
-          "https://d1irmm0k9ptj7n.cloudfront.net/2019-04-01-episode-4.mp3"
+          "https://media.haskell-weekly.com/2019-04-01-episode-4.mp3"
     <*> HW.Type.Date.gregorianToDate 2019 4 1
     <*> HW.Type.Duration.timestampToDuration 15 57
     <*> HW.Type.Guid.textToGuid "aea8101c-b126-4cb5-be14-00200d3f6c27"
@@ -169,7 +169,7 @@ episode5 =
     <$> HW.Type.Articles.textsToArticles
           ["https://sakshamsharma.com/2018/03/haskell-proj-struct/"]
     <*> HW.Type.Audio.textToAudio
-          "https://d1irmm0k9ptj7n.cloudfront.net/2019-04-08-episode-5.mp3"
+          "https://media.haskell-weekly.com/2019-04-08-episode-5.mp3"
     <*> HW.Type.Date.gregorianToDate 2019 4 8
     <*> HW.Type.Duration.timestampToDuration 15 15
     <*> HW.Type.Guid.textToGuid "25b43cdb-e278-42da-97dc-3c6d353ec8c8"
@@ -185,7 +185,7 @@ episode6 =
     <$> HW.Type.Articles.textsToArticles
           ["https://functor.tokyo/blog/2019-04-07-ghcid-for-web-app-dev"]
     <*> HW.Type.Audio.textToAudio
-          "https://d1irmm0k9ptj7n.cloudfront.net/2019-04-15-episode-6.mp3"
+          "https://media.haskell-weekly.com/2019-04-15-episode-6.mp3"
     <*> HW.Type.Date.gregorianToDate 2019 4 15
     <*> HW.Type.Duration.timestampToDuration 18 38
     <*> HW.Type.Guid.textToGuid "7ed15199-bcd3-461e-af62-d504ae8a4a01"
@@ -202,7 +202,7 @@ episode7 =
           [ "https://williamyaoh.com/posts/2019-04-11-cheatsheet-to-regexes-in-haskell.html"
           ]
     <*> HW.Type.Audio.textToAudio
-          "https://d1irmm0k9ptj7n.cloudfront.net/2019-04-22-episode-7.mp3"
+          "https://media.haskell-weekly.com/2019-04-22-episode-7.mp3"
     <*> HW.Type.Date.gregorianToDate 2019 4 22
     <*> HW.Type.Duration.timestampToDuration 17 29
     <*> HW.Type.Guid.textToGuid "287a197e-e9fd-47b6-9506-2f39be002af7"
@@ -219,7 +219,7 @@ episode8 =
           [ "https://medium.com/co-star-engineering/continuous-improvement-with-hlint-code-smells-e490886558a1"
           ]
     <*> HW.Type.Audio.textToAudio
-          "https://d1irmm0k9ptj7n.cloudfront.net/2019-04-29-episode-8.mp3"
+          "https://media.haskell-weekly.com/2019-04-29-episode-8.mp3"
     <*> HW.Type.Date.gregorianToDate 2019 4 29
     <*> HW.Type.Duration.timestampToDuration 14 20
     <*> HW.Type.Guid.textToGuid "53bbcaeb-6e6f-4e1f-9806-f24032ac7a9f"
@@ -236,7 +236,7 @@ episode9 =
           [ "https://medium.com/daml-driven/four-tweaks-to-improve-haskell-b1de9c87f816"
           ]
     <*> HW.Type.Audio.textToAudio
-          "https://d1irmm0k9ptj7n.cloudfront.net/2019-05-06-episode-9.mp3"
+          "https://media.haskell-weekly.com/2019-05-06-episode-9.mp3"
     <*> HW.Type.Date.gregorianToDate 2019 5 6
     <*> HW.Type.Duration.timestampToDuration 21 52
     <*> HW.Type.Guid.textToGuid "de704aad-e6a1-41a6-976f-bd3f2ef34ad2"
@@ -253,7 +253,7 @@ episode10 =
           [ "https://blog.ploeh.dk/2016/03/18/functional-architecture-is-ports-and-adapters/"
           ]
     <*> HW.Type.Audio.textToAudio
-          "https://d1irmm0k9ptj7n.cloudfront.net/2019-05-20-episode-10.mp3"
+          "https://media.haskell-weekly.com/2019-05-20-episode-10.mp3"
     <*> HW.Type.Date.gregorianToDate 2019 5 20
     <*> HW.Type.Duration.timestampToDuration 16 37
     <*> HW.Type.Guid.textToGuid "32fd3459-b349-4c99-9150-5073fedab6bf"
@@ -269,7 +269,7 @@ episode11 =
     <$> HW.Type.Articles.textsToArticles
           ["https://blog.jez.io/profiling-in-haskell/"]
     <*> HW.Type.Audio.textToAudio
-          "https://d1irmm0k9ptj7n.cloudfront.net/2019-05-27-episode-11.mp3"
+          "https://media.haskell-weekly.com/2019-05-27-episode-11.mp3"
     <*> HW.Type.Date.gregorianToDate 2019 5 27
     <*> HW.Type.Duration.timestampToDuration 19 12
     <*> HW.Type.Guid.textToGuid "3ec1dc79-7a9c-46c3-b919-61471e876708"
@@ -285,7 +285,7 @@ episode12 =
     <$> HW.Type.Articles.textsToArticles
           ["https://www.tweag.io/posts/2019-05-27-ormolu.html"]
     <*> HW.Type.Audio.textToAudio
-          "https://d1irmm0k9ptj7n.cloudfront.net/2019-06-03-episode-12.mp3"
+          "https://media.haskell-weekly.com/2019-06-03-episode-12.mp3"
     <*> HW.Type.Date.gregorianToDate 2019 6 3
     <*> HW.Type.Duration.timestampToDuration 16 37
     <*> HW.Type.Guid.textToGuid "f166f89f-1a16-49f1-915a-d54505c301a0"
@@ -302,7 +302,7 @@ episode13 =
           [ "https://github.com/github/semantic/blob/eaf13783838861fe5eb6cd46d59354774a8eb88d/docs/why-haskell.md"
           ]
     <*> HW.Type.Audio.textToAudio
-          "https://d1irmm0k9ptj7n.cloudfront.net/2019-06-10-episode-13.mp3"
+          "https://media.haskell-weekly.com/2019-06-10-episode-13.mp3"
     <*> HW.Type.Date.gregorianToDate 2019 6 10
     <*> HW.Type.Duration.timestampToDuration 25 8
     <*> HW.Type.Guid.textToGuid "fb192c3c-02a5-4413-ab53-1346677940ec"
@@ -319,7 +319,7 @@ episode14 =
           [ "https://danieljharvey.github.io/posts/2019-07-05-refined-types.html"
           ]
     <*> HW.Type.Audio.textToAudio
-          "https://d1irmm0k9ptj7n.cloudfront.net/2019-07-16-episode-14.mp3"
+          "https://media.haskell-weekly.com/2019-07-16-episode-14.mp3"
     <*> HW.Type.Date.gregorianToDate 2019 7 16
     <*> HW.Type.Duration.timestampToDuration 15 18
     <*> HW.Type.Guid.textToGuid "5ec19b21-9399-474b-be54-beadd37894f9"
@@ -335,7 +335,7 @@ episode15 =
     <$> HW.Type.Articles.textsToArticles
           ["https://treszkai.github.io/2019/07/13/haskell-eval"]
     <*> HW.Type.Audio.textToAudio
-          "https://d1irmm0k9ptj7n.cloudfront.net/2019-07-23-episode-15.mp3"
+          "https://media.haskell-weekly.com/2019-07-23-episode-15.mp3"
     <*> HW.Type.Date.gregorianToDate 2019 7 23
     <*> HW.Type.Duration.timestampToDuration 18 13
     <*> HW.Type.Guid.textToGuid "a76ba20a-49f7-4a5f-a40d-bffb34417b2d"
@@ -369,7 +369,7 @@ episode16 =
           , "https://github.com/meteficha/Hipmunk"
           ]
     <*> HW.Type.Audio.textToAudio
-          "https://d1irmm0k9ptj7n.cloudfront.net/2019-07-30-episode-16.mp3"
+          "https://media.haskell-weekly.com/2019-07-30-episode-16.mp3"
     <*> HW.Type.Date.gregorianToDate 2019 7 30
     <*> HW.Type.Duration.timestampToDuration 12 54
     <*> HW.Type.Guid.textToGuid "0bbb483d-d256-4311-b2bc-2a98a0c0eaad"
@@ -385,7 +385,7 @@ episode17 =
     <$> HW.Type.Articles.textsToArticles
           ["https://typeclasses.com/news/2019-07-phrasebook"]
     <*> HW.Type.Audio.textToAudio
-          "https://d1irmm0k9ptj7n.cloudfront.net/2019-08-06-episode-17.mp3"
+          "https://media.haskell-weekly.com/2019-08-06-episode-17.mp3"
     <*> HW.Type.Date.gregorianToDate 2019 8 6
     <*> HW.Type.Duration.timestampToDuration 13 56
     <*> HW.Type.Guid.textToGuid "df526ec2-5d4e-4c1a-b4b5-eca8b6918731"
@@ -401,7 +401,7 @@ episode18 =
     <$> HW.Type.Articles.textsToArticles
           ["https://blog.kabir.sh/posts/inventing-monads.html"]
     <*> HW.Type.Audio.textToAudio
-          "https://d1irmm0k9ptj7n.cloudfront.net/2019-08-13-episode-18.mp3"
+          "https://media.haskell-weekly.com/2019-08-13-episode-18.mp3"
     <*> HW.Type.Date.gregorianToDate 2019 8 13
     <*> HW.Type.Duration.timestampToDuration 16 41
     <*> HW.Type.Guid.textToGuid "153162fd-b6f5-40f7-8b05-fe20b91b702b"
@@ -418,7 +418,7 @@ episode19 =
           [ "https://thomashoneyman.com/articles/practical-profunctor-lenses-optics/"
           ]
     <*> HW.Type.Audio.textToAudio
-          "https://d1irmm0k9ptj7n.cloudfront.net/2019-08-20-episode-19.mp3"
+          "https://media.haskell-weekly.com/2019-08-20-episode-19.mp3"
     <*> HW.Type.Date.gregorianToDate 2019 8 20
     <*> HW.Type.Duration.timestampToDuration 18 56
     <*> HW.Type.Guid.textToGuid "bbd19a71-7d79-4514-8acb-a5729cdd26c6"
@@ -434,7 +434,7 @@ episode20 =
     <$> HW.Type.Articles.textsToArticles
           ["https://typeclasses.com/featured/rounding"]
     <*> HW.Type.Audio.textToAudio
-          "https://d1irmm0k9ptj7n.cloudfront.net/2019-09-13-episode-20.mp3"
+          "https://media.haskell-weekly.com/2019-09-13-episode-20.mp3"
     <*> HW.Type.Date.gregorianToDate 2019 9 13
     <*> HW.Type.Duration.timestampToDuration 15 49
     <*> HW.Type.Guid.textToGuid "d916811b-886b-4da7-a104-ff65bda7124c"
@@ -450,7 +450,7 @@ episode21 =
     <$> HW.Type.Articles.textsToArticles
           ["https://www.well-typed.com/blog/2019/09/eventful-ghc/"]
     <*> HW.Type.Audio.textToAudio
-          "https://d1irmm0k9ptj7n.cloudfront.net/2019-10-07-episode-21.mp3"
+          "https://media.haskell-weekly.com/2019-10-07-episode-21.mp3"
     <*> HW.Type.Date.gregorianToDate 2019 10 7
     <*> HW.Type.Duration.timestampToDuration 14 46
     <*> HW.Type.Guid.textToGuid "dc731681-016f-4380-8acc-18877ba41abe"
@@ -467,7 +467,7 @@ episode22 =
           [ "http://h2.jaguarpaw.co.uk/posts/good-design-and-type-safety-in-yahtzee/"
           ]
     <*> HW.Type.Audio.textToAudio
-          "https://d1irmm0k9ptj7n.cloudfront.net/2019-10-14-episode-22.mp3"
+          "https://media.haskell-weekly.com/2019-10-14-episode-22.mp3"
     <*> HW.Type.Date.gregorianToDate 2019 10 14
     <*> HW.Type.Duration.timestampToDuration 31 41
     <*> HW.Type.Guid.textToGuid "e6e8718a-19f5-4a0b-8962-f2fb08cd1432"
@@ -490,7 +490,7 @@ episode23 =
           , "https://github.com/gelisam/frp-zoo"
           ]
     <*> HW.Type.Audio.textToAudio
-          "https://d1irmm0k9ptj7n.cloudfront.net/2019-10-30-episode-23.mp3"
+          "https://media.haskell-weekly.com/2019-10-30-episode-23.mp3"
     <*> HW.Type.Date.gregorianToDate 2019 10 30
     <*> HW.Type.Duration.timestampToDuration 20 18
     <*> HW.Type.Guid.textToGuid "1492e4e4-4897-402f-b953-796870946ebd"
@@ -509,7 +509,7 @@ episode24 =
           , "https://taylor.fausak.me/2017/11/15/2017-state-of-haskell-survey-results/"
           ]
     <*> HW.Type.Audio.textToAudio
-          "https://d1irmm0k9ptj7n.cloudfront.net/2019-11-08-episode-24.mp3"
+          "https://media.haskell-weekly.com/2019-11-08-episode-24.mp3"
     <*> HW.Type.Date.gregorianToDate 2019 11 8
     <*> HW.Type.Duration.timestampToDuration 7 57
     <*> HW.Type.Guid.textToGuid "ec19bb5d-f9e9-4bf6-b4e4-d055a3d747f0"
@@ -530,7 +530,7 @@ episode25 =
           , "https://www.youtube.com/watch?v=pwnrfREbhWY"
           ]
     <*> HW.Type.Audio.textToAudio
-          "https://d1irmm0k9ptj7n.cloudfront.net/2020-10-08-episode-25.mp3"
+          "https://media.haskell-weekly.com/2020-10-08-episode-25.mp3"
     <*> HW.Type.Date.gregorianToDate 2020 10 8
     <*> HW.Type.Duration.timestampToDuration 33 5
     <*> HW.Type.Guid.textToGuid "474c84d6-4364-408d-91e9-94eaf08945d2"
@@ -546,7 +546,7 @@ episode26 = HW.Type.Episode.Episode
     [ "https://dev.to/sshine/getting-recursively-drunk-with-monoids-2jek"
     ]
   <*> HW.Type.Audio.textToAudio
-    "https://d1irmm0k9ptj7n.cloudfront.net/2020-10-13-episode-26.mp3"
+    "https://media.haskell-weekly.com/2020-10-13-episode-26.mp3"
   <*> HW.Type.Date.gregorianToDate 2020 10 13
   <*> HW.Type.Duration.timestampToDuration 18 51
   <*> HW.Type.Guid.textToGuid "4b9f812e-8a9f-45c2-b221-f2b8f8d5270c"
