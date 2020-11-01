65d351c84b6a640aaec9d50c3561f1fb493f8675
Taylor Fausak <taylor@fausak.me>
2020-10-31 21:13:57 -0400

    Add more information to the completion page

--- src/lib/HW/Template/SurveyComplete.hs
+++ src/lib/HW/Template/SurveyComplete.hs
@@ -14,6 +14,9 @@ template baseUrl =
   Base.template baseUrl "2020 Survey :: Haskell Weekly" mempty $ do
     Html.h2_ [Html.class_ "f2 mv3 tracked-tight"] "2020 Survey"
     Html.p_ $ do
-      "Thank you for filling out the 2020 State of Haskell Survey!"
+      "Thank you for filling out the 2020 State of Haskell Survey! "
+      "Please help us get an accurate view of the Haskell community by sharing the survey."
+    Html.p_ "If you'd like to be notified about the results, please sign up for our newsletter:"
     Newsletter.callToAction baseUrl
+    Html.p_ "You may also be interested in our podcast:"
     Podcast.callToAction baseUrl
