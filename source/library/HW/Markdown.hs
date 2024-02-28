8b5c47492fac6051244bec80c71d699b0933affd
Taylor Fausak <taylor@fausak.me>
2024-02-28 13:14:02 -0600

    Add IDs to headings (#285)
    * Add IDs to headings and paragraphs
    
    * Generate slugs for headings

--- source/library/HW/Markdown.hs
+++ source/library/HW/Markdown.hs
@@ -6,6 +6,7 @@ module HW.Markdown
 where
 
 import qualified CMark
+import qualified Data.Char as Char
 import qualified Data.Text as Text
 
 type Markdown = CMark.Node
@@ -14,9 +15,36 @@ fromText :: Text.Text -> Markdown
 fromText = CMark.commonmarkToNode options
 
 toHtml :: Markdown -> Text.Text
-toHtml = CMark.nodeToHtml []
+toHtml = CMark.nodeToHtml [CMark.optUnsafe] . addIds
 
 -- | Options only need to be applied when parsing Markdown, not when rendering
 -- HTML.
 options :: [CMark.CMarkOption]
 options = [CMark.optNormalize, CMark.optSafe, CMark.optSmart]
+
+addIds :: Markdown -> Markdown
+addIds n = case n of
+  CMark.Node p t ns -> CMark.Node p t $ case t of
+    CMark.HEADING _ ->
+      let a =
+            CMark.Node
+              Nothing
+              (CMark.HTML_INLINE $ "<a id='" <> toSlug (getText n) <> "'></a>")
+              []
+       in a : ns
+    _ -> fmap addIds ns
+
+getText :: Markdown -> [Text.Text]
+getText n = case n of
+  CMark.Node _ t ns -> case t of
+    CMark.TEXT text -> [text]
+    _ -> concatMap getText ns
+
+toSlug :: [Text.Text] -> Text.Text
+toSlug =
+  let p c = Char.isAsciiLower c || Char.isDigit c || c == '_'
+   in Text.intercalate "-"
+        . Text.words
+        . Text.map (\c -> if p c then c else ' ')
+        . Text.toLower
+        . Text.unwords
