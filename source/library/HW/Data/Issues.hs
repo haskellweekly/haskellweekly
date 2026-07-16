4aee5c84d7bc728d99c071d1e1e9045b04a3a3d1
Alexey Zabelin <hello@alexeyzabelin.com>
2026-07-15 23:46:11 -0700

    Publish issue 533

--- source/library/HW/Data/Issues.hs
+++ source/library/HW/Data/Issues.hs
@@ -537,5 +537,6 @@ all =
     Issue.Issue <$> Date.fromGregorian 2026 6 18 <*> Number.fromNatural 529,
     Issue.Issue <$> Date.fromGregorian 2026 6 25 <*> Number.fromNatural 530,
     Issue.Issue <$> Date.fromGregorian 2026 7 2 <*> Number.fromNatural 531,
-    Issue.Issue <$> Date.fromGregorian 2026 7 9 <*> Number.fromNatural 532
+    Issue.Issue <$> Date.fromGregorian 2026 7 9 <*> Number.fromNatural 532,
+    Issue.Issue <$> Date.fromGregorian 2026 7 16 <*> Number.fromNatural 533
   ]
